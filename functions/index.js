ec8d34da51a9fd93f97727ee7c10f343cb92c618
Ricardo Hui <eehaha@gmail.com>
2018-06-06 16:24:27 +0800

    set up cloud fn using firebase with simple endpoint and connection to db

---
+++ functions/index.js
@@ -0,0 +1,33 @@
+const functions = require('firebase-functions');
+
+const admin = require('firebase-admin');
+admin.initializeApp();
+// // Create and Deploy Your First Cloud Functions
+// // https://firebase.google.com/docs/functions/write-firebase-functions
+//
+exports.addMessage = functions.https.onRequest((request, response) => {
+  const content = request.query.text;
+
+  return admin
+    .database()
+    .ref('/messages')
+    .push({ orignal: content })
+    .then(snapshot => {
+      return response.redirect(303, snapshot.ref.toString());
+    });
+});
+
+// Listens for new messages added to /messages/:pushId/original and creates an
+// uppercase version of the message to /messages/:pushId/uppercase
+exports.makeUppercase = functions.database
+  .ref('/messages/{pushId}/original')
+  .onCreate((snapshot, context) => {
+    // Grab the current value of what was written to the Realtime Database.
+    const original = snapshot.val();
+    console.log('Uppercasing', context.params.pushId, original);
+    const uppercase = original.toUpperCase();
+    // You must return a Promise when performing asynchronous tasks inside a Functions such as
+    // writing to the Firebase Realtime Database.
+    // Setting an "uppercase" sibling in the Realtime Database returns a Promise.
+    return snapshot.ref.parent.child('uppercase').set(uppercase);
+  });
